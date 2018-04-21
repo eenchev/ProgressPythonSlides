be0e6785885b99368e77c37e56efdbbbcf57d878
Enchev <Evgeni.Enchev@ataccama.com>
2018-04-21 18:01:24 +0300

    Fix run_slides script

--- lectures_notebooks/run_slides.py
+++ lectures_notebooks/run_slides.py
@@ -3,8 +3,19 @@ import sys
 import platform
 import argparse
 
+try:
+    from pip import main as pipmain
+except:
+    from pip._internal import main as pipmain
+
+try:
+    import jupyter
+except ImportError:
+    pipmain(['install', 'jupyter'])
+
 parser=argparse.ArgumentParser(
-    description='''Simple script to run ipynb notebooks as slides ''')
+    description='''Simple script to run ipynb notebooks as slides. \
+            Installs jupyter if not present  ''')
 parser.add_argument('--file', type=str, default='1.Python101.ipynb', 
     help='Specifies the target file. If left empty runs 1.Python101.ipynb')
 args=parser.parse_args()
