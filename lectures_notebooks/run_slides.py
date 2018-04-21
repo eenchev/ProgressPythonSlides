be70ec6f1d8b46d3b366bff867d0b3c8aee7e1a8
Enchev <Evgeni.Enchev@ataccama.com>
2018-04-21 18:34:35 +0300

    Fix few edge cases in run_slides

--- lectures_notebooks/run_slides.py
+++ lectures_notebooks/run_slides.py
@@ -15,13 +15,24 @@ except ImportError:
 
 parser=argparse.ArgumentParser(
     description='''Simple script to run ipynb notebooks as slides. \
-            Installs jupyter if not present  ''')
+            Installs jupyter if not present.''')
 parser.add_argument('--file', type=str, default='1.Python101.ipynb', 
     help='Specifies the target file. If left empty runs 1.Python101.ipynb')
-args=parser.parse_args()
+args, unknown = parser.parse_known_args()
 
+if len(unknown)>1:
+    parser.print_help()
+    print("\nYou passed extra arguments. Did you try to pass filename?")
+    print("Try the --file argument. Please refer to the help above.")
+    sys.exit(1)
 
-command_string = 'jupyter nbconvert {} --to slides --post serve'.format(args.file)
+elif len(unknown) == 1:
+    file_name = unknown[0]
+
+else:
+    file_name = args.file
+
+command_string = 'jupyter nbconvert {} --to slides --post serve'.format(file_name)
 os_platform = platform.system()
 
 if os_platform == 'Windows':
